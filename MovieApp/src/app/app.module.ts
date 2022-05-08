b3d39821c6188bf5f7b513d8910aceec7c92d6ce
aaron <azewudie@uwm.edu>
2022-05-08 14:56:49 -0500

    added some comment on the login page

--- MovieApp/src/app/app.module.ts
+++ MovieApp/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import {LoginPageComponent} from './login-page/login-page.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ MovieApp/src/app/login-page/login-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.sass']
+})
+export class LoginPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  data = {
+    username:"Aaron",
+    password:"1234"
+  };
+  signUser(){
+    if(this.data.username ==="Aaron" && this.data.password ==="1234"){
+
+      console.log("Hello Aaron!")
+    }else{
+    console.log("please signup an account");
+    }
+   
+    
+  }
+  handleClear(){
+    this.data.username = ' '; 
+    this.data.password = ' ';
+   }
+   saveValues(){
+    this.data.username = this.data.username; 
+    this.data.password =  this.data.password;
+   }
+}
