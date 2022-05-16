ca2d3e1956983ca6aea6727d4f5e763808c8c7be
Priyanka Gnanasekaran <priya.nat52@gmail.com>
2022-05-15 21:05:34 -0400

    merging

--- MovieApp/src/app/app.module.ts
+++ MovieApp/src/app/app.module.ts
@@ -6,17 +6,11 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 import { LoginPageComponent } from './login-page/login-page.component'
-<<<<<<< HEAD
 import { RegisterComponent } from './register-page/register-page.component';
-=======
-import { RegisterComponent } from './register/register.component';
->>>>>>> 1eba0b4c7ba4585764bb5e4e1964f6ef80c0cfb5
 import { HomePageComponent } from './home-page/home-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MovieListComponent } from './movie-list/movie-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
+
 import { MovieCardComponent } from './movie-list/movie-card/movie-card.component'
 import { MatInputModule } from '@angular/material/input';
 import { TextReductionPipe } from './pipes/text-reduction.pipe';
@@ -63,16 +57,8 @@ const routes: Routes = [
     MovieListComponent,
     MovieCardComponent,
     TextReductionPipe,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatIconModule,
-    MatInputModule
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
