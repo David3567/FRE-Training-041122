8631ef3454f04b18d41b07389afa5bcc177c830a
yoshua <yoshua.carrera24@gmail.com>
2022-05-23 14:50:14 -0400

    Removed cached Environment

--- MovieApp/src/app/pipes/text-reduction.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextReductionPipe } from './text-reduction.pipe';
-
-describe('TextReductionPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TextReductionPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
