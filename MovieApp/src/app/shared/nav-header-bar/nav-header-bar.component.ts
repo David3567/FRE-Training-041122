a5094c1c0c5cee1d097fa831ce3318cc91c8103e
Priyanka Gnanasekaran <priya.nat52@gmail.com>
2022-06-05 22:37:14 -0400

    Removing redubdant component

--- MovieApp/src/app/shared/nav-header-bar/nav-header-bar.component.ts
+++ MovieApp/src/app/shared/nav-header-bar/nav-header-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { AuthLocalStorageService } from 'src/app/services/auth-local-storage.service';
 
 @Component({
   selector: 'app-nav-header-bar',
@@ -9,13 +11,32 @@ import { MatDrawer } from '@angular/material/sidenav';
 export class NavHeaderBarComponent implements OnInit {
 
   @Output() public sidenavToggle = new EventEmitter();
-  
-  constructor() { }
+  isLogin!: boolean;
+  username = '';
+  welcome = ('Welcome ' + this.username + ' !! ');
+
+  constructor(private readonly authService: AuthLocalStorageService, private readonly router: Router) { }
 
   ngOnInit(): void {
+    const { JWToken, username } = this.authService.userValue;
+    
+    if (JWToken && username) {
+      this.isLogin = true;
+      this.username = username;
+      this.welcome = ('Welcome ' + this.username + '!! ');
+    } else {
+      this.isLogin = false;
+    }
   }
 
   public onToggleSidenav = () => {
     this.sidenavToggle.emit();
   }
+
+  logOut(){
+    this.authService.logoutAuth();
+    this.isLogin = false;
+    this.username = '';
+  }
+  
 }
--- MovieApp/src/app/shared/shared.module.ts
+++ MovieApp/src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavHeaderBarComponent } from 'src/app/shared/nav-header-bar/nav-header-bar.component';
-import { NavLoggedInHeaderComponent } from 'src/app/shared/nav-logged-in-header/nav-logged-in-header.component';
 import { NavSidebarComponent } from 'src/app/shared/nav-sidebar/nav-sidebar.component';
 import { TextReductionPipe } from 'src/app/pipes/text-reduction.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -36,14 +35,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   declarations: [
     NavHeaderBarComponent,
-    NavLoggedInHeaderComponent,
     NavSidebarComponent,
     TextReductionPipe,
   ],
   exports: [
     CommonModule,
     NavHeaderBarComponent,
-    NavLoggedInHeaderComponent,
     NavSidebarComponent,
     TextReductionPipe,
     RouterModule,
